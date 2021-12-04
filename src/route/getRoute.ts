156efc1a46f99b24c83f4a57d0e81372fef62193
konsoue <874111752@qq.com>
2021-12-04 16:37:59 +0800

    feat(route): 修改路由和添加header组件

--- src/route/getRoute.ts
+++ src/route/getRoute.ts
@@ -3,7 +3,22 @@ import { IpagesRoutes } from './types'
 import { is, injectUnmount } from '@/Utils'
 
 class RouteMethod {
-  requirePage() {
+  isChild(path: string, url: string): boolean {
+    let pLen = path.length, uLen = url.length;
+    if (uLen === pLen && url === '/') return true;
+    if (uLen < pLen) return false;
+    if (!url.includes(path)) return false;
+    let restUrl = url.replace(path, '');
+    const reg = /^[a-zA-Z]+$/;
+    if (restUrl[0] === '/') restUrl = restUrl.replace('/', '');
+    return reg.test(restUrl);
+  }
+  /**
+   * 获取对应 pages/path 路径下的 的 index.tsx
+   * @param path
+   * @returns
+   */
+  requirePage(path: string = '/'): IpagesRoutes[] {
     const pagesCtx = require.context('@/pages/', true, /index.tsx/);
     const pagesRoutes: IpagesRoutes[] = [];
     let lastRoute: IpagesRoutes = {};
@@ -16,16 +31,20 @@ class RouteMethod {
       if (url === '/home') url = '/';
       else if (url.includes('/home')) url = url.replace('/home', '');
       else if (url === '/notFound') {
-        lastRoute = { url, component, exact: false }
+        lastRoute = { url: '*', component, exact: false }
         continue;
       }
-      pagesRoutes.push({
-        url,
-        exact: true,
-        component
-      })
+      // 处理 path 参数
+      if (path[0] !== '/') path = '/' + path;
+      if (this.isChild(path, url)) {
+        pagesRoutes.push({
+          url,
+          exact: true,
+          component
+        })
+      }
     }
-    pagesRoutes.push(lastRoute);
+    if (path === '/') pagesRoutes.push(lastRoute);
     return pagesRoutes;
   }
 }
