179b3a66f2f6a6b377c82e52bbad5a8beb8b2fde
wenny mei <383776758.@qq.com>
2021-12-07 19:56:50 +0800

    feat(myCollection):添加我的收藏功能

---
+++ src/pages/user/myCollection/types.tsx
@@ -0,0 +1,13 @@
+
+export interface IResponseResult {
+  data?: any;
+}
+
+export interface INewsList {
+  _id: string;
+  digest: string;
+  img: string;
+  publishTime: string;
+  source: string;
+  title: string;
+}
--- src/pages/user/myLikes/index.tsx
+++ src/pages/user/myLikes/index.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from '@/hooks/useFetch';
+import { likeUrl } from '@/Utils/urls'
+import { List } from '@arco-design/web-react';
 
-const index: React.FC = () => {
-  return <div>user</div>;
+
+const MyLikes: React.FC = () => {
+
+  const { run: getMyLikes } = useFetch({
+    url: likeUrl.getMyLikes,
+    type: 'GET',
+  })
+  useEffect(() => {
+    getMyLikes({
+      size: '10',
+      current: '1'
+    })
+      .then((res) => {
+        const data = res.data;
+        console.log(data)
+      })
+      .catch((err) => { })
+  }, [])
+
+  return (<List
+    style={{ width: 1000 }}
+    size='large'
+    render={(item, index) => <List.Item key={index}>{item}</List.Item>}
+  />)
 };
-export default index;
+export default MyLikes;
