0ddd98d053f6c874e2ba1ed6dbbe3fc2d7970e51
remy-cpu <3062857934@qq.com>
2021-12-08 22:57:56 +0800

    feat(changeStyle): 修改主题色和字体大小

--- src/pages/user/myReport/index.tsx
+++ src/pages/user/myReport/index.tsx
@@ -1,13 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-12-06 21:39:41
- * @LastEditTime: 2021-12-07 17:08:00
+ * @LastEditTime: 2021-12-08 22:54:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \byte_project\src\pages\user\myReport\index.tsx
  */
 import React from "react";
-import '@/theme.scss' 
+import '@/theme.scss'
 import './index.scss'
 import { LS } from '@/Utils'
 
@@ -16,10 +16,12 @@ const index: React.FC = () => {
   // 用户可看看到自己使用最关注的频道是哪个，每个频道的浏览量
   // 最常使用 app 的时间段等等
   const theme = LS.getItem('useDark')
+  const fontSize = LS.getItem('fontSize')
+  const themeColor = LS.getItem('themeColor')
   return (
-  <div className={`report-page ${theme}`}>
-    user
-  </div>
+    <div className={`report-page ${theme} ${fontSize} ${themeColor}`}>
+      user
+    </div>
   )
 };
 export default index;
--- src/pages/user/personal/index.tsx
+++ src/pages/user/personal/index.tsx
@@ -1,24 +1,25 @@
 /*
  * @Author: your name
  * @Date: 2021-12-06 21:39:41
- * @LastEditTime: 2021-12-07 16:44:08
- * @LastEditors: your name
+ * @LastEditTime: 2021-12-08 20:29:46
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \byte_project\src\pages\user\personal\index.tsx
  */
 import React from "react";
-import { Button } from '@arco-design/web-react'
-import '@/theme.scss' 
+import '@/theme.scss'
 import './index.scss'
 import { LS } from '@/Utils'
 const index: React.FC = () => {
   // 修改头像
   // 修改昵称
   const theme = LS.getItem('useDark')
+  const fontSize = LS.getItem('fontSize')
+  const themeColor = LS.getItem('themeColor')
   return (
-  <div className={`personal-page ${theme}`}>
-    <div>djsiajds </div>
-  </div>
+    <div className={`personal-page ${theme} ${fontSize} ${themeColor}`}>
+      <div>哈哈哈哈哈</div>
+    </div>
   );
 };
 export default index;
