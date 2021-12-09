f1aa6fa6c708728d32985b3ede7b3f22ccd77652
3119004730黄创境 <1026903031@qq.com>
2021-12-09 23:24:35 +0800

    style(header) : 头部样式更改

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -1,28 +1,39 @@
-import React, { memo, useMemo } from 'react';
-import PubSearch from '@/common/PubSearch';
-import PubAvatar from './PubAvatar'
-import NewTabs from './NewTabs'
-import { IHeaderProps, IPubAvatarProps } from './types'
-import localStorageUtils from '@/Utils/localStorageUtils'
-import './index.scss'
+import React, { memo, useMemo } from "react";
+import PubSearch from "@/common/PubSearch";
+import PubAvatar from "./PubAvatar";
+import NewTabs from "./NewTabs";
+import { IHeaderProps, IPubAvatarProps } from "./types";
+import localStorageUtils from "@/Utils/localStorageUtils";
+import { history } from "@/route";
+import "./index.scss";
 
 const Header: React.FC<IHeaderProps> = (props) => {
   const { toFlash, flash } = props;
 
   const loginData: IPubAvatarProps = useMemo(() => {
     const data = localStorageUtils.get();
-    if (JSON.stringify(data) === '{}') {
-      return { login: false }
+    if (JSON.stringify(data) === "{}") {
+      return { login: false };
     } else {
-      return { login: true, avatarUrl: data.user.avatar }
+      return { login: true, avatarUrl: data.user.avatar };
     }
-  }, [flash])
+  }, [flash]);
 
   return (
     <div className="pub-header-container">
       <div className="pub-header">
         <div className="header-left">
-          <div className="logo"><div className="title-font"></div></div>
+          <div className="logo">
+            <div
+              className="title-font"
+              style={{
+                cursor:'pointer'
+              }}
+              onClick={() => {
+                history.push("/");
+              }}
+            ></div>
+          </div>
           <NewTabs toFlash={toFlash} />
         </div>
         <div className="header-middle">
@@ -37,8 +48,7 @@ const Header: React.FC<IHeaderProps> = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default memo(Header)
+export default memo(Header);
